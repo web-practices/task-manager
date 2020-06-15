b1195f7c51877dafa6e997d078d873a406b10dae
Eason Li <lijinghua@odc.cmbchina.cn>
2020-06-15 15:31:29 +0800

    Add modal popover

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+function showMedalPopover() {
+  let modal = document.getElementById('modal');
+  modal.classList.add('modal-flex-center');
+}
+
+function hideMedalPopover() {
+  let modal = document.getElementById('modal');
+  modal.classList.remove('modal-flex-center');
+}
